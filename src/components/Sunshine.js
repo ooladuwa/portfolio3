20ea4784bc620a5045028d0cb596eec5aaf0fb15
Onaje Oladuwa <onaje.oladuwa@gmail.com>
2021-03-16 01:13:14 -0400

    routing complete/tweaking main page styling

--- src/components/Sunshine.js
+++ src/components/Sunshine.js
@@ -8,19 +8,7 @@ const Sunshine = () => {
         src="https://i.ibb.co/H20Bv0t/sunshine.png"
         alt="sunshine"
         className="sunshine-img"
-        usemap="#image-map"
       />
-      
-      <map name="image-map">
-        <area
-          target="_blank"
-          alt="LinkedIn"
-          title="LinkedIn"
-          href="https://www.linkedin.com/in/ooladuwa/"
-          coords="2356,2352,444"
-          shape="circle"
-        />
-      </map>
       <Roots />
     </section>
   );
