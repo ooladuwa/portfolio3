2b3950af9104ee84d6b6594a042b1d05552e7ac4
Onaje Oladuwa <onaje.oladuwa@gmail.com>
2021-03-17 10:14:50 -0400

    social buttons functional

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,16 +1,28 @@
-import React from 'react'
-import { FaGithubSquare } from "react-icons/fa"
-import { FaLinkedin } from "react-icons/fa"
-import { FaTwitterSquare } from "react-icons/fa"
+import React from "react";
+import { FaGithubSquare } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaTwitterSquare } from "react-icons/fa";
 
 const Socials = () => {
   return (
     <div className="socials-container">
-      <FaGithubSquare className="socialIcon" size={42} />
-      <FaLinkedin className="socialIcon" size={42} />
-      <FaTwitterSquare className="socialIcon" size={42} />
+      <a href="https://www.github.com/ooladuwa" target="_blank">
+        <button className="socials-button">
+          <FaGithubSquare className="socialIcon" size={42} />
+        </button>
+      </a>
+      <a href="https://www.linkedin.com/in/ooladuwa/" target="_blank">
+        <button className="socials-button">
+          <FaLinkedin className="socialIcon" size={42} />
+        </button>
+      </a>
+      <a href="https://twitter.com/ooladuwa" target="_blank">
+        <button className="socials-button">
+          <FaTwitterSquare className="socialIcon" size={42} />
+        </button>
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default Socials
+export default Socials;
